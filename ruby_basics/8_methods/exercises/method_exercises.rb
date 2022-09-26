89898bc01d7d252e5da1d38413da46704bea1d97
Mattias Welamsson <mattiaswelamsson@SmackDH-MBP.local>
2022-09-26 15:57:51 +0900

    lucky number method complete

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -2,27 +2,39 @@
 # To gain more familiarity, look up the documentation for each hint.
 # Remember to unskip the corresponding tests one at a time.
 
+
 # method name: #ascii_translator
 # parameter: number (an integer)
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
+def ascii_translator(number)
+    number.chr
+end
 
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
+def common_sports(current_sports, favorite_sports)
+    current_sports.intersection(favorite_sports)
+end
 
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
-
+def alphabetical_list(games)
+    games.sort.uniq!
+end
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
-# return value: a string "Today's lucky number is <number>"
+# return value: a stri =ng "Today's lucky number is <number>"
+def lucky_number(number = 7)
+    "Today's lucky number is #{number}"
+end
 
 
 # method name: #ascii_code
@@ -31,6 +43,14 @@
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
 
+def ascii_code(character)
+    character = character.to_s
+    if character.ord != 1
+        return "Input Error"
+    else 
+        return
+    end
+end
 
 # method name: #pet_pun
 # parameter: animal (a string)
