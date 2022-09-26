51fcb79170d4807734c2f412754ce04751489a3a
Mattias Welamsson <mattias@iMac.local>
2022-09-27 08:57:25 +0900

    greatest_value completed

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -10,7 +10,7 @@ def decrement_smallest_value(nested_array)
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -24,7 +24,7 @@ def increment_greatest_value(nested_array)
   greatest_value = nested_array.flatten.min
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < current_value
         greatest_value = current_value
       end
     end
@@ -35,11 +35,14 @@ end
 # This next exercise might look familiar
 # Use p and puts in order to find what's wrong with our method
 
+
 def isogram?(string)
+
   original_length = string.length
   string_array = string.downcase.split
-  unique_length = string_array.uniq.length
+  unique_length = string_array.uniq
   original_length == unique_length
+
 end
 
 # Can you guess what's next?
